fd7f367d1241d90c5b447f34bba16be7e12a1f39
Eugenia Valls <elisadiazartcontact@gmail.com>
2023-04-24 21:23:09 +0200

    24.04.23

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,48 @@
+import { useState } from "react";
+
 const App = () => {
-	return <h1>Núcleo de la aplicación</h1>;
+	const {counter, increment, decrement, incrementStep, reset} = useApp()
+
+	return <>
+	<h1>useReducer() example</h1>
+	<h2>value: {counter.value}</h2>
+	<h2>step: {counter.step}</h2>
+	<h2>clicks: {counter.click}</h2>
+	<button onClick={()=>increment()}>Incrementar</button>
+	<button onClick={()=> decrement()}>Decrementar</button>
+	<button onClick={() => incrementStep()}>Incrementar Step</button>
+	<button onClick={() => reset()}>Reset</button>
+	</>
 };
+
+
+
+const useApp = () => {
+	const[counter, setCounter]= useState({
+		value: 0,
+		step: 1,
+		click: 0
+	})
+
+	const increment = () => {
+		setCounter({...counter, value: counter.value + counter.step, click: counter.click+1})
+	}
+	
+	const decrement = () => {
+		setCounter({...counter,value: counter.value - counter.step, click: counter.click+1})
+	}
+	
+	const incrementStep = () => {
+		setCounter({...counter,step: counter.step + 1})
+	}
+	
+	const reset = () => {
+		setCounter({ value: 0, click: 0, step: 1})
+	}
+
+	return {counter, increment, decrement, incrementStep, reset}
+}
+
+
 
 export default App;
